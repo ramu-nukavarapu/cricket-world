eeabf6abe5a94e968df5092fa2ce4782b48b6750
ramu <ramunukavarapu2004@gmail.com>
2024-06-11 19:05:45 +0530

    fix the background issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'home-pc': "url('../public/assets/homePC.jpg')",
-        'home-mob': "url('../public/assets/homeMOB.jpeg')",
-        'vs-pc': "url('../public/assets/vsPC.jpeg')",
-        'vs-mob': "url('../public/assets/vsMOB.jpeg')",
-        'board-pc': "url('../public/assets/boardPC.jpeg')",
-        'board-mob': "url('../public/assets/boardMOB.jpeg')",
-      }
+      
     },
   },
   plugins: [],
